ddf9b81a263ad4e1fc4047bfb7f102581524ac2e
Schaffer Krisztian <schaffer.krisztian@gmail.com>
2020-07-25 11:40:21 +0200

    Performance improvement

--- src/ObjectPools.jl
+++ src/ObjectPools.jl
@@ -4,22 +4,20 @@ export allocate, release
 
 const MAX_POOL_SIZE = 100
 
-const pools = Dict{Symbol, Vector}()
+const pools = Dict{Type, Vector}()
 
 function createpool(type::Type{T}) where T
     pool = Vector{type}()
-    pools[Symbol(string(T))] = pool
+    pools[T] = pool
     return pool
 end
 
 @generated function getpool(type::Type{T}) where T
-    s = Symbol(string(T))
-    if !haskey(pools, s)
+    if !haskey(pools, T)
         createpool(T)
     end
-    return quote
-        return pools[$(Base.Meta.quot(s))]
-    end
+    retval = pools[T]
+    return :($retval)
 end
 
 function allocate(type::Type{T}, args...)::T where T
